a1cc7666c3e7d437e25754687f3929592d48fe22
Aysha-sithar <asitharahts@qatar.com>
2023-12-09 12:52:03 +0000

    Fixed get_token bug

--- gauth/gauth/gauth.py
+++ gauth/gauth/gauth.py
@@ -19,7 +19,6 @@ error='Authentication required. Please provide valid credentials..'
 def getToken2(self):
     pass
 
-from frappe.integrations.oauth2 import get_token
 
 @frappe.whitelist(allow_guest=True)
 def test_api():
@@ -41,8 +40,7 @@ def generate_token_secure( api_key, api_secret, app_key):
         
         client_id = clientID  # Replace with your OAuth client ID
         client_secret = clientSecret  # Replace with your OAuth client secret
-        # url = "https://dev.claudion.com/api/method/employee_app.gauth.get_token"
-        url =  frappe.local.conf.host_name  + "/api/method/employee_app.gauth.get_token"
+        url =  frappe.local.conf.host_name  + "/api/method/frappe.integrations.oauth2.get_token"
         payload = {
             "username": api_key,
             "password": api_secret,
@@ -76,7 +74,6 @@ def generate_custom_token(username, password):  #Used for development testing on
         clientID, clientSecret, clientUser = frappe.db.get_value('OAuth Client', {'app_name': 'MobileAPP'}, ['client_id', 'client_secret','user'])
         client_id = clientID  # Replace with your OAuth client ID
         client_secret = clientSecret  # Replace with your OAuth client secret
-        # url = "https://dev.claudion.com/api/method/employee_app.gauth.get_token"
         url =  frappe.local.conf.host_name  + "/api/method/employee_app.gauth.get_token"
         payload = {
             "username": username,
@@ -106,7 +103,6 @@ def generate_custom_token_for_employee( password):
        
         client_id = clientID  # Replace with your OAuth client ID
         client_secret = clientSecret  # Replace with your OAuth client secret
-        # url = "https://dev.claudion.com/api/method/employee_app.gauth.get_token"
         url =  frappe.local.conf.host_name  + "/api/method/employee_app.gauth.get_token"
         payload = {
             # "username": username,
