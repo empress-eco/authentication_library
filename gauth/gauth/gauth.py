75bdf12eab975a0b94ec6de1ffd7efc524622293
fkalpana <support@erpgulf.com>
2024-01-12 23:06:59 +0300

    added twilio and experttexting on company settings

--- gauth/gauth/gauth.py
+++ gauth/gauth/gauth.py
@@ -18,6 +18,10 @@ from frappe import _, is_whitelisted, ping
 from frappe.utils import (
 	now_datetime
 )
+from frappe.utils import add_days, flt
+
+from erpnext.accounts.report.financial_statements import get_data, get_period_list
+from erpnext.accounts.utils import get_balance_on, get_fiscal_year
 
 error='Authentication required. Please provide valid credentials..'
 
@@ -68,6 +72,51 @@ def generate_token_secure( api_key, api_secret, app_key):
             # frappe.local.response.http_status_code = 401
             # return json.loads(response.text)
             return Response(json.dumps({"message": e , "user_count": 0}), status=500, mimetype='application/json')
+        
+        
+        
+@frappe.whitelist(allow_guest=True)
+def generate_token_secure_for_users( username, password, app_key):
+    try:
+        try:
+            app_key = base64.b64decode(app_key).decode("utf-8")
+        except Exception as e:
+            return Response(json.dumps({"message": "Security Parameters are not valid" , "user_count": 0}), status=401, mimetype='application/json')
+        clientID, clientSecret, clientUser = frappe.db.get_value('OAuth Client', {'app_name': app_key}, ['client_id', 'client_secret','user'])
+        
+        if clientID is None:
+            # return app_key
+            return Response(json.dumps({"message": "Security Parameters are not valid" , "user_count": 0}), status=401, mimetype='application/json')
+        
+        client_id = clientID  # Replace with your OAuth client ID
+        client_secret = clientSecret  # Replace with your OAuth client secret
+        url =  frappe.local.conf.host_name  + "/api/method/frappe.integrations.oauth2.get_token"
+        payload = {
+            "username": username,
+            "password": password,
+            "grant_type": "password",
+            "client_id": client_id,
+            "client_secret": client_secret,
+            # "grant_type": "refresh_token"
+        }
+        files = []
+        headers = {"Content-Type": "application/json"}
+        response = requests.request("POST", url, data=payload, files=files)
+        if response.status_code == 200:
+            response_data = json.loads(response.text)
+            response_data['Email'],response_data['Full_name'], response_data['Phone_number'],response_data['QID'] = _get_customer_details(user_email=username)      
+            return response_data
+        else:
+            frappe.local.response.http_status_code = 401
+            return json.loads(response.text)
+            
+    except Exception as e:
+            # frappe.local.response.http_status_code = 401
+            # return json.loads(response.text)
+            return Response(json.dumps({"message": e , "user_count": 0}), status=500, mimetype='application/json')
+
+
+
 
 @frappe.whitelist(allow_guest=True)
 def generate_custom_token(username, password):  #Used for development testing only. not for production
@@ -198,7 +247,7 @@ def is_user_available(user_email = None, mobile_phone = None):
             
 
 @frappe.whitelist()
-def g_create_user(full_name, password, mobile_no, email,role=None):
+def g_create_user(full_name, password, mobile_no, email, id,role=None):
     # return "inside g_create_user"
     # return check_user_name(user_email=email, mobile_phone=mobile_no)
     if(check_user_name(user_email=email, mobile_phone=mobile_no)>0):
@@ -212,6 +261,17 @@ def g_create_user(full_name, password, mobile_no, email,role=None):
                         "email": email,
                         "roles": [{"role": role}]
                         }).insert()
+        
+        frappe.get_doc({"doctype":"Customer",
+                        "name":email,
+                        "customer_name": email,
+                        "custom_user": email,
+                        "custom_full_name":full_name,
+                        "custom_mobile_number":mobile_no,
+                        "email": email,
+                        "custom_qid": id,
+                        }).insert()
+        
         
         return g_generate_reset_password_key(email, send_email=False, password_expired=False)
 
@@ -396,4 +456,169 @@ def upload_file():
                                 
     return urls
 
+@frappe.whitelist(allow_guest=True)
+def send_sms_expertexting(phone_number,otp):
+    try:
+        phone_number = "+974" + phone_number
+        url = "https://www.experttexting.com/ExptRestApi/sms/json/Message/Send"
 
+        payload = f'username={get_sms_id("experttexting")}&from=DEFAULT&to={phone_number}&text=Your%20validation%20code%20for%20DallahMzad%20is%20{otp}%20Thank%20You.'
+        headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        if response.status_code  in (200,201 ):
+            return True
+        else:
+            return False
+        
+        
+    except Exception as e:
+        frappe.throw("Error in qr sending SMS   " + str(e))   
+
+
+@frappe.whitelist(allow_guest=True)
+def send_sms_twilio(phone_number,otp):
+    # success response = 201 created
+    try:
+        import requests
+        phone_number = "+974" + phone_number
+        url = f"https://api.twilio.com/2010-04-01/Accounts/{get_sms_id('twilio')}/Messages.json"
+        payload = f'To={phone_number}&From=+18789999387&Body=Your%20DallahMzad%20Validation%20%20is%20{otp}%20Please%20use%20it%20on%20the%20site%20or%20App'
+        headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic QUNiZmQyMDRkNDEwMTg4MmY2YTIwYzRlMDYxMzZkMmJiMzoyZmE4MjVkNGE2Mzk2MjYxYmNkMDVjOWZkMzAxZTI1OQ=='
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        if response.status_code  in (200,201 ):
+            return True
+        else:
+            return response.text
+         
+        # if response.status_code  in (400,405,406,409 ):
+        
+    except Exception as e:
+        return "Error in qr sending SMS   " + str(e)
+
+@frappe.whitelist(allow_guest=True)
+def payment_gateway_log(reference,amount,user,bid):
+                    try:
+                        current_time = frappe.utils.now()
+                        frappe.get_doc({
+                            "doctype": "payment_gateway_initiated",
+                            "reference": reference,
+                            "date_time": current_time,
+                            "amount": amount,
+                            "user": user,
+                            "bid": bid
+                        }).insert(ignore_permissions=True)
+                        return "Successfully logged Payment gateway initialization"
+                    except Exception as e:
+                        frappe.log_error(title='Payment logging failed',message=frappe.get_traceback())
+                        return "Error in payment gateway log  " + str(e)
+                    
+
+def get_sms_id(provider):
+    default_company = frappe.db.get_single_value("Global Defaults", "default_company")
+    if provider == "twilio":
+        return frappe.db.get_value("Company", default_company, "custom_twilio_id")
+    if provider == "experttexting":
+        return frappe.db.get_value("Company", default_company, "custom_experttexting_id")
+    
+
+@frappe.whitelist(allow_guest=True)
+def make_payment_entry(
+	amount,
+    user,
+	bid,
+    reference
+):
+
+	if amount == 0:
+		return "Amount not correct"
+
+	journal_entry = frappe.new_doc("Journal Entry")
+	journal_entry.posting_date = frappe.utils.now()
+	journal_entry.company = frappe.db.get_single_value("Global Defaults", "default_company")
+	journal_entry.voucher_type = "Journal Entry"
+
+	debit_entry = {
+		"account": "QIB Account - D",
+		"credit": amount,
+		"credit_in_account_currency": amount,
+		"account_currency": "QAR",
+		"reference_name": None,
+		"reference_type": "Journal Entry",
+		"reference_detail_no": "",
+		"cost_center": "",
+		"project": "",
+	}
+
+	credit_entry = {
+		"account": "1310 - Debtors - D",
+		"debit": amount,
+		"debit_in_account_currency": amount,
+		"account_currency": "QAR",
+		"reference_name": None,
+		"reference_type": "Journal Entry",
+		"reference_detail_no": "",
+		"cost_center": "",
+		"project": "",
+	}
+
+	# for dimension in get_accounting_dimensions():
+	# 	debit_entry.update({dimension: item.get(dimension)})
+
+	# 	credit_entry.update({dimension: item.get(dimension)})
+
+	journal_entry.append("accounts", debit_entry)
+	journal_entry.append("accounts", credit_entry)
+
+	try:
+		journal_entry.save(ignore_permissions=True)
+
+		# if submit:
+		# 	journal_entry.submit()
+
+		frappe.db.commit()
+	except Exception as e:
+            frappe.db.rollback()
+            frappe.log_error(title='Payment Entry failed to JV',message=frappe.get_traceback())
+            frappe.flags.deferred_accounting_error = True
+            return  Response(json.dumps({"data": "null", "message": str(e) }), status=400, mimetype='application/json')
+
+@frappe.whitelist()
+def get_customer_details(user_email = None, mobile_phone = None):
+    if mobile_phone is not None:
+        customer_details = frappe.get_list('Customer', filters={'custom_mobile_number': mobile_phone}, fields=["name as email", "enabled","custom_full_name as full_name","custom_mobile_number as mobile_number","custom_qid as qid"] )
+    elif user_email is not None:
+        customer_details = frappe.get_list('Customer', filters={'name': user_email}, fields=["name as email", "enabled","custom_full_name as full_name","custom_mobile_number as mobile_number","custom_qid as qid"] )
+    else:
+        return  Response(json.dumps({"message": "Customer not found" , "user_count": 0}), status=404, mimetype='application/json')
+    
+    if len(customer_details) >=1:
+        return  customer_details
+    else:
+        return  Response(json.dumps({"message": "Customer not found" , "user_count": 0}), status=404, mimetype='application/json')
+    
+def _get_customer_details(user_email = None, mobile_phone = None):
+    if mobile_phone is not None:
+        customer_details = frappe.get_list('Customer', filters={'custom_mobile_number': mobile_phone}, fields=["name as email", "enabled","custom_full_name as full_name","custom_mobile_number as mobile_number","custom_qid as qid"] )
+    elif user_email is not None:
+        customer_details = frappe.get_list('Customer', filters={'name': user_email}, fields=["name as email", "enabled","custom_full_name as full_name","custom_mobile_number as mobile_number","custom_qid as qid"] )
+    else:
+        return  Response(json.dumps({"message": "Customer not found" , "user_count": 0}), status=404, mimetype='application/json')
+    
+    if len(customer_details) >=1:
+        return  customer_details[0]['email'], customer_details[0]['full_name'], customer_details[0]['mobile_number'], customer_details[0]['qid']
+    else:
+        return  Response(json.dumps({"message": "Customer not found" , "user_count": 0}), status=404, mimetype='application/json')
+    
+@frappe.whitelist(allow_guest=True)
+def get_account_balance(customer):
+    return  get_balance_on(party_type="Customer", party=customer)
+
+    
